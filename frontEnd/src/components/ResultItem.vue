f0f33640947fc02d44d22ac1685f2061e6c7f8ae
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-27 11:42:52 -0600

    vue search bar

---
+++ frontEnd/src/components/ResultItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="result-item">
+     
+        <h4>test</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+/* Add your result item styles here */
+.result-item {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+</style>
---
+++ frontEnd/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
