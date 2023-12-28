f9fc4ff44e43ebe7d8f1f119ad9579e7788e0a16
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-28 17:38:28 -0600

    create the cors policy

--- api/main.go
+++ api/main.go
@@ -9,6 +9,18 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
+
 	v1 := r.Group("/api/v1")
 
 	v1.GET("/", func(c *gin.Context) {
