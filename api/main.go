2dd8930505f98253ba53cc1313dd97c1feb91a85
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-28 14:30:07 -0600

    setting up the api router group and creating the search route with the search function

--- api/main.go
+++ api/main.go
@@ -9,10 +9,23 @@ import (
 
 func main() {
 	r := gin.Default()
+	v1 := r.Group("/api/v1")
 
-	r.GET("/", func(c *gin.Context) {
-		result := zinc.SearchDocument("Gold")
-		c.JSON(http.StatusOK, result)
+	v1.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Enron-Email Index ZincSearch API v1",
+		})
+	})
+
+	v1.GET("/search", func(c *gin.Context) {
+		searchTerm := c.Query("word")
+		if searchTerm == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Query parameter 'word' is required"})
+			return
+		}
+		result := zinc.SearchDocument(searchTerm)
+		c.JSON(http.StatusAccepted, result)
+
 	})
 
 	r.Run(":3000")
