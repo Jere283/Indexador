5812d5100f0d8ce958422800efd707be62c058db
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-28 17:39:01 -0600

    integrate the result item component to show the information from the api

--- frontEnd/src/components/ResultItem.vue
+++ frontEnd/src/components/ResultItem.vue
@@ -1,20 +1,42 @@
 <template>
-    <div class="result-item">
-     
-        <h4>test</h4>
+  <div>
+    <h1>Search Results</h1>
+    <div v-for="result in searchResults" :key="result._id">
+      <div>
+        <strong>ID:</strong> {{ result._id }}
+      </div>
+      <div>
+        <strong>Score:</strong> {{ result._score }}
+      </div>
+      <div>
+        <strong>Source:</strong> {{ JSON.stringify(result._source, null, 2) }}
+      </div>
+      <!-- Add more fields as needed -->
     </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+export default {
+  setup() {
+    const searchResults = ref([]);
+
+    const searchUSA = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/v1/search/usa');
+        searchResults.value = response.data.hits.hits;
+        console.log(searchResults.value[0]._index);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      }
+    };
+
+    onMounted(searchUSA);
+
+    return { searchResults };
+  },
+};
 </script>
-
-<style scoped>
-/* Add your result item styles here */
-.result-item {
-  margin: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-}
-</style>
