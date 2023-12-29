2337571f96dd2bbe843b404ad2c77f8fa8a09d54
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-29 02:16:00 -0600

    create a modal to show the body of the search results

---
+++ frontEnd/src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    v-if="modelValue"
+    class="fixed z-10 inset-0 overflow-y-auto bg-gray-500 bg-opacity-50"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+  >
+    <!-- Modal content -->
+    <div class="modal-content">
+      <button @click="closeModal" class="close-button">X</button>
+      <!--full content -->
+      <p class="modal-text">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["update:modelValue"],
+  props: ["modelValue", "content"],
+  methods: {
+    closeModal() {
+      this.$emit("update:modelValue", false);
+    },
+  },
+};
+</script>
+<style>
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  margin: 50px auto;
+  max-width: 500px;
+  position: relative;
+}
+
+.close-button {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+}
+
+.modal-text {
+  white-space: pre-wrap; /* CSS3 */
+  white-space: -moz-pre-wrap; /* Firefox */
+  white-space: -pre-wrap; /* Opera <7 */
+  white-space: -o-pre-wrap; /* Opera 7 */
+  word-wrap: break-word; /* IE */
+}
+</style>
--- frontEnd/src/components/ResultItem.vue
+++ frontEnd/src/components/ResultItem.vue
@@ -1,26 +1,44 @@
 <template>
-
-  <div class="mt-5" >
+  <div class="mt-5">
     <div class="px-4 sm:px-8 max-w-5xl m-auto">
-        <h1 class="text-center font-semibold text-sm">Search results: {{ searchResults.length }}</h1>
-        <ul class="border border-transparent rounded overflow-hidden shadow-md">
-            <li  v-for="result in searchResults" :key="result._id" 
-            class=" mt-4 px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-transparent rounded transition-all duration-300 ease-in-out">
-            <strong>From:</strong> {{ result._source.From  }}  <strong>To:</strong> {{ result._source.To }}  <strong>Subject:</strong> {{ result._source.Subject }} </li>
-        </ul>
+      <h1 class="text-center font-semibold text-sm">
+        Search results: {{ searchResults.length }}
+      </h1>
+      <ul class="border border-transparent rounded overflow-hidden shadow-md">
+        <li
+          v-for="result in searchResults"
+          :key="result._id"
+          @click="openModal(result)"
+          class="mt-4 px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-transparent rounded transition-all duration-300 ease-in-out"
+        >
+          <strong>From:</strong> {{ result._source.From }} <strong>To:</strong>
+          {{ result._source.To }} <strong>Subject:</strong>
+          {{ result._source.Subject }}
+        </li>
+      </ul>
     </div>
+    <Modal
+      :modelValue="showModal"
+      @update:modelValue="(val) => (showModal = val)"
+      :content="modalContent"
+    />
   </div>
-
 </template>
 
 <script>
 import { ref, watch } from "vue";
 import axios from "axios";
+import Modal from "./Modal.vue";
 
 export default {
+  components: {
+    Modal,
+  },
   props: ["searchTerm"],
   setup(props) {
     const searchResults = ref([]);
+    const showModal = ref(false);
+    const modalContent = ref("");
 
     const search = async (term) => {
       try {
@@ -28,11 +46,14 @@ export default {
           `http://localhost:3000/api/v1/search/${term}`
         );
         searchResults.value = response.data.hits.hits;
-
-        console.log(searchResults.value[5]._source);
       } catch (error) {
         console.error("Error fetching search results:", error);
       }
+    };
+
+    const openModal = (result) => {
+      showModal.value = true;
+      modalContent.value = JSON.stringify(result._source, null, 2);
     };
 
     watch(
@@ -44,8 +65,7 @@ export default {
       }
     );
 
-    return { searchResults };
+    return { searchResults, showModal, modalContent, openModal };
   },
 };
-
 </script>
