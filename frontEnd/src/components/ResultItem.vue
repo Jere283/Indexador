79b389c1caf84c94df82c649f4a3b771c4d06a75
Jere283 <jeremyfigueroa2015@outlook.com>
2023-12-29 01:10:18 -0600

    changes on the results UI

--- frontEnd/src/components/ResultItem.vue
+++ frontEnd/src/components/ResultItem.vue
@@ -1,52 +1,16 @@
 <template>
 
-<div class="container" v-for="result in searchResults" :key="result._id">
-  <div class="min-h-screen flex items-center justify-center px- mt-4 mb-4">
-    <div class="max-w-4xl bg-white w-full rounded-lg shadow-xl">
-      <div class="p-4 border-b">
-        <h2 class="text-2xl">Email Information</h2>
-        <p class="text-sm text-gray-500">Message Id: {{ result._source.Message_id }} </p>
-      </div>
-      <div>
-        <div
-          class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b"
-        >
-          <p class="text-gray-600">Subject</p>
-          <p>{{ result._source.Subject }}</p>
-        </div>
-        <div
-          class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b"
-        >
-          <p class="text-gray-600">Date</p>
-          <p>{{ result._source.Date }}</p>
-        </div>
-        <div
-          class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b"
-        >
-          <p class="text-gray-600">From</p>
-          <p>{{ result._source.From }}</p>
-        </div>
-        <div
-          class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b"
-        >
-          <p class="text-gray-600">To</p>
-          <p>{{ result._source.To }}</p>
-        </div>
-        <div
-          class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b"
-        >
-          <p class="text-gray-600">Body</p>
-          <p>
-            {{ result._source.body }}
-          </p>
-        </div>
-        
-      </div>
+  <div class="mt-5" >
+    <div class="px-4 sm:px-8 max-w-5xl m-auto">
+        <h1 class="text-center font-semibold text-sm">Search results: {{ searchResults.length }}</h1>
+        <ul class="border border-transparent rounded overflow-hidden shadow-md">
+            <li  v-for="result in searchResults" :key="result._id" 
+            class=" mt-4 px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-transparent rounded transition-all duration-300 ease-in-out">
+            <strong>From:</strong> {{ result._source.From  }}  <strong>To:</strong> {{ result._source.To }}  <strong>Subject:</strong> {{ result._source.Subject }} </li>
+        </ul>
     </div>
   </div>
-</div>
-  
-  
+
 </template>
 
 <script>
@@ -65,7 +29,7 @@ export default {
         );
         searchResults.value = response.data.hits.hits;
 
-        console.log(searchResults.value[0]._source);
+        console.log(searchResults.value[5]._source);
       } catch (error) {
         console.error("Error fetching search results:", error);
       }
@@ -84,16 +48,4 @@ export default {
   },
 };
 
-var i = 0;
 </script>
-
-<style>
-.container {
-  max-width: 100%;  /* Ensure the container does not exceed the viewport width */
-  overflow: hidden; /* Hide any content that overflows the container */
-  word-wrap: break-word; /* Break long words and prevent overflow */
-  /* Add additional styles as needed */
-}
-
-/* Add any other styles for your page layout */
-</style>
